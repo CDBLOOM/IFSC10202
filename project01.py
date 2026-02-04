e78f35ff8f9f87c1a454bd5b1e015265d4e5f903
CDBLOOM <cdbloomingburg@ualr.edu>
2026-02-04 18:02:21 +0000

    debugged

--- project01.py
+++ project01.py
@@ -1,26 +1,28 @@
 # This program prompts for a length of time in seconds. 
 
-import math
-
 total_seconds = int(input("Enter Length of Time in Seconds: "))
 
 seconds_in_minutes = 60 
-seconds_in_hour = 60
+seconds_in_hour = 3600
 seconds_in_day = 86400
 seconds_in_year = 31536000
 
-year(s) = total_seconds // seconds_in_year
+years = total_seconds // seconds_in_year
+total_seconds -= years * seconds_in_year
 
-day(s) = total_seconds // seconds_in_day
+days = total_seconds // seconds_in_day
+total_seconds -= days * seconds_in_day
 
-hour(s) = total_seconds // seconds_in_hour
+hours = total_seconds // seconds_in_hour
+total_seconds -= hours * seconds_in_hour
 
-minute(s) = total_seconds // seconds_in_minutes
+minutes = total_seconds // seconds_in_minutes
+total_seconds -= minutes * seconds_in_minutes
 
 seconds = total_seconds
 
-print("Year(s):", year(s))
-print("Day(s0:", day(s))
-print("Hour(s):", hour(s))
-print("Minute(s):", minute(s))
-print("Second(s):", second(s))
+print("Years:", years)
+print("Days:", days)
+print("Hours:", hours)
+print("Minutes:", minutes)
+print("Seconds:", seconds)
